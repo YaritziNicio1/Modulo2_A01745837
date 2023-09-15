cc63f55c870a7650be9e4405c96af21ab0b9c718
YaritziNicio1 <132979863+YaritziNicio1@users.noreply.github.com>
2023-09-15 09:25:10 -0600

    Add files via upload

--- Framework.py
+++ Framework.py
@@ -22,30 +22,30 @@ X_temp, X_valid, y_temp, y_valid = train_test_split(X, y, test_size=0.2, random_
 #Se divide de nuevo, ahora en un conjunto de entrenamiento y validación con el conjunto temporal anterior 
 X_train_c, X_test_c, y_train_c, y_test_c = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
-# Vvisualización de dispersión de los datos, conlas características de petal width y petal lenght
+# Vvisualización de dispersión de los datos, con las características de petal width y petal lenght
 plt.figure()
 plt.scatter(X[:,3] , X[:,2] , c = y , s=20)
 plt.title("Dispersión de los datos")
 plt.show()
 
 
-#Visualización de los datos de entrenamiento, validación y prueba
+#Visualización de los datos de entrenamiento, validación y prueba con las características de petal width y petal lenght
 #Train
-plt.subplot(131)
+plt.subplot(131) #Esta función nos ayuda a tener las 3 gráficas en una sola pantalla, de esta manera es más fácil comparar
 plt.scatter(X_train_c[:, 3], X_train_c[:, 2], c=y_train_c, s=20)
 plt.title('Conjunto de Entrenamiento')
 plt.xlabel('Longitud del pétalo')
 plt.ylabel('Ancho del pétalo')
 
 #Test
-plt.subplot(132)
+plt.subplot(132)  #Esta función nos ayuda a tener las 3 gráficas en una sola pantalla, de esta manera es más fácil comparar
 plt.scatter(X_test_c[:, 3], X_test_c[:, 2], c=y_test_c, s=20)
 plt.title('Conjunto de prueba')
 plt.xlabel('Longitud del pétalo')
 plt.ylabel('Ancho del pétalo')
 
 #Validation
-plt.subplot(133)
+plt.subplot(133)  #Esta función nos ayuda a tener las 3 gráficas en una sola pantalla, de esta manera es más fácil comparar
 plt.scatter(X_valid[:, 3], X_valid[:, 2], c=y_valid, s=20)
 plt.title('Conjunto de vaidación')
 plt.xlabel('Longitud del pétalo')
@@ -56,10 +56,13 @@ plt.show()
 #Definimos el número de k vecinos cercanos a implementar en el modelo 
 k = [3,6,9]
 
+#Imprimimos el valor a utilizar
 print("*Train* Usando un valor de k=3")
 # Impolementación del modelo KNN con los valores de K correspondientes
 clf = KNeighborsClassifier(n_neighbors=3)
+#Se hace un ajuste del modelo a los datos a utilizar *train*
 clf.fit(X_train_c, y_train_c)
+#Predicciones con los datos de entrenamiento y un k=3
 predic_t3 = clf.predict(X_train_c)
 print("Se obtienen las siguientes predicciones: ",predic_t3)
 
@@ -74,23 +77,29 @@ print(accuracy)
 
 # Calculamos el F1 Score
 f1_t = f1_score(y_train_c, predic_t3, average='weighted')
+#Imprimimos el resultado
 print("Puntaje F1:", f1_t)
 
 #Se calcula el MSE
 mse_t = mean_squared_error(y_train_c, predic_t3)
+#Imprimimos el resultado
 print("MSE:", mse_t)
 
 # Se hace una matriz de confusión
 confusion_train = confusion_matrix(y_train_c, predic_t3)
 print("Matriz de Confusión: ")
+#Imprimimos la matriz
 print(confusion_train)
 
 #--------------------------------------------------------------------------------------------------------
 #Ahora, se utiliza un valor de k=6
+#Imprimimos el valor a utilizar
 print("*Train* Usando un valor de k=6")
 # Impolementación del modelo KNN con los valores de K correspondientes
 clf = KNeighborsClassifier(n_neighbors=6)
+#Se hace un ajuste del modelo a los datos a utilizar *train*
 clf.fit(X_train_c, y_train_c)
+#Predicciones con los datos de entrenamiento y un k=6
 predic_t6 = clf.predict(X_train_c)
 print("Se obtienen las siguientes predicciones: ",predic_t6)
 
@@ -105,24 +114,30 @@ print(accuracy6)
 
 # Calculamos el F1 Score
 f1_t6 = f1_score(y_train_c, predic_t6, average='weighted')
+#Imprimimos el resultado
 print("Puntaje F1:", f1_t6)
 
 #Se calcula el MSE
 mse_t6 = mean_squared_error(y_train_c, predic_t6)
+#Imprimimos el resultado
 print("MSE:", mse_t6)
 
 # Se hace una matriz de confusión
 confusion_t6 = confusion_matrix(y_train_c, predic_t6)
 print("Matriz de Confusión: ")
+#Imprimimos la matriz
 print(confusion_t6)
 
 
 #------------------------------------------------------------------------------------------------------------------
 #Por último, se utiliza un valor de k=9
+#Imprimimos el valor a utilizar
 print("*Train* Usando un valor de k=9")
 # Impolementación del modelo KNN con los valores de K correspondientes
 clf = KNeighborsClassifier(n_neighbors=9)
+#Se hace un ajuste del modelo a los datos a utilizar *train*
 clf.fit(X_train_c, y_train_c)
+#Predicciones con los datos de entrenamiento y un k=9
 predic_t9 = clf.predict(X_train_c)
 print("Se obtienen las siguientes predicciones: ",predic_t9)
 
@@ -133,29 +148,36 @@ correct_predictions9 = np.sum(predic_t9 == y_train_c)
 total_predictions9 = len(y_train_c)
     #Calculo de las predicciones correctas entre el total de predicciones, asi se obtiene el accuracy 
 accuracy9 = correct_predictions9 / total_predictions9
+#Imprimimos el resultado
 print(accuracy9)
 
 # Calculamos el F1 Score
 f1_t9 = f1_score(y_train_c, predic_t9, average='weighted')
+#Imprimimos el resultado
 print("Puntaje F1:", f1_t9)
 
 #Se calcula el MSE
 mse_t9 = mean_squared_error(y_train_c, predic_t9)
+#Imprimimos el resultado
 print("MSE:", mse_t9)
 
 # Se hace una matriz de confusión
 confusion_t9 = confusion_matrix(y_train_c, predic_t9)
 print("Matriz de Confusión: ")
+#Imprimimos la matriz
 print(confusion_t9)
 
 #Se hace el mismo procedimiento para los datos de validation y test 
     #Se involucra un ciclo for 
 for i in range(len(k)):
     #****Para datos de validación********* 
+    #Se imprime el valor a utilizar
     print("*Validación* Usando un valor de k={0}".format(k[i]))
     # Implementación del modelo KNN con los valores de K correspondientes
     clf = KNeighborsClassifier(n_neighbors=k[i])
+    #Se hace un ajuste del modelo a los datos a utilizar *validación*
     clf.fit(X_valid, y_valid)
+    #Predicciones con los datos de validación y diferentes valores de k 
     predict_v = clf.predict(X_valid)
     print("Se obtienen las siguientes predicciones: ",predict_v)
     #Se calcula el accuracy del modelo
@@ -169,15 +191,18 @@ for i in range(len(k)):
 
     # Calculamos el F1 Score
     f1_v = f1_score(y_valid, predict_v, average='weighted')
+    #Imprimimos el resultado
     print("Puntaje F1:", f1_v)
 
     #Se calcula el MSE
     mse_v = mean_squared_error(y_valid, predict_v)
+    #Imprimimos el resultado
     print("MSE:", mse_v)
 
     # Se hace una matriz de confusión
     confusion_v = confusion_matrix(y_valid, predict_v)
     print("Matriz de Confusión: ")
+    #Imprimimos la matriz
     print(confusion_v)
 
 
@@ -185,7 +210,9 @@ for i in range(len(k)):
     print("*Test* Usando un valor de k={0}".format(k[i]))
     # Implementación del modelo KNN con los valores de K correspondientes
     clf = KNeighborsClassifier(n_neighbors=k[i])
+    #Se hace un ajuste del modelo a los datos a utilizar *test*
     clf.fit(X_test_c, y_test_c)
+    #Predicciones con los datos de prueba y diferentes valores de k 
     predict_t = clf.predict(X_test_c)
     print("Se obtienen las siguientes predicciones: ",predict_t)
     #Se calcula el accuracy del modelo
@@ -199,56 +226,18 @@ for i in range(len(k)):
 
     # Calculamos el F1 Score
     f1_te = f1_score(y_test_c, predict_t, average='weighted')
+    #Imprimimos el resultado
     print("Puntaje F1:", f1_te)
 
     #Se calcula el MSE
     mse_te = mean_squared_error(y_test_c, predict_t)
+    #Imprimimos el resultado
     print("MSE:", mse_te)
 
     # Se hace una matriz de confusión
     confus_te = confusion_matrix(y_test_c, predict_t)
     print("Matriz de Confusión: ")
+    #Imprimimos la matriz
     print(confus_te)
 
     print("\n\n")
-
-
-#***
-
-'''
-for i in range(len(k)):
-    # Impolementación del modelo KNN con los valores de K correspondientes
-    clf = KNeighborsClassifier(n_neighbors=k[i])
-
-    # Ajuste del modelo k-NN a los datos de entrenamiento
-    clf.fit(X_train_c, y_train_c)
-
-    #Imprimimos una frase que muestre el valor de k que estamos usando
-    print("Usando valor de k={0}\n".format(k_values[i]))
-
-    # Realización de predicciones en los datos de entrenamiento
-    predictions_train = clf.predict(X_train)
-
-    # Impresión de las predicciones
-    print("Predicciones con datos de entrenamiento:")
-    print(predictions_train)
-
-
-# Escala tus datos (esto es importante para KNN)
-scaler_reg = StandardScaler()
-X_train_reg = scaler_reg.fit_transform(X_train_reg)
-X_test_reg = scaler_reg.transform(X_test_reg)
-
-# Crea una instancia del regresor KNN y ajústalo a tus datos de entrenamiento
-knn_regressor = KNeighborsRegressor(n_neighbors=3)  # Puedes ajustar el número de vecinos según tus necesidades
-knn_regressor.fit(X_train_reg, y_train_reg)
-
-# Realiza predicciones en los datos de prueba y evalúa el rendimiento
-y_pred_reg = knn_regressor.predict(X_test_reg)
-mse = mean_squared_error(y_test_reg, y_pred_reg)
-print(f"Error cuadrático medio (MSE) del regresor KNN: {mse}")
-
-#Se definen todas las métricas a obtener }
-
-'''
-
